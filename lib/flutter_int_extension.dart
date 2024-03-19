ff92fd9f7abdb8f101b3cb16b2e6fa1835fb2964
Bockelandt Adrien <a.bockelandt@cosmediterranee.com>
2024-03-19 09:09:38 +0100

    Add isBetween method & convert getters

--- lib/flutter_int_extension.dart
+++ lib/flutter_int_extension.dart
@@ -1,7 +1,14 @@
 library flutter_int_extension;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+extension IntExtension on int {
+  String get toBinary => toRadixString(2);
+  String get toHex => toRadixString(16);
+
+  bool isBetween(int min, int max, {bool canBeEgal = false}) {
+    if (canBeEgal && (this == min || this == max)) {
+      return true;
+    }
+
+    return min < this && this < max;
+  }
 }
